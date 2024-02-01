60e012204d107e9fcf0cdf893673f31c8bc38191
hadi14250 <hadihk2k@gmail.com>
2024-02-01 15:05:46 +0400

    done

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CR_PAT=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
-echo $CR_PAT | docker login ghcr.io -u scomcoin --password-stdin
-docker compose -f docker-compose-dev.yml up
-docker logout
-docker compose -f docker-compose-dev.yml down

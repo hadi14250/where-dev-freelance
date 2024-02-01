51a8a098e0463f80e24133e0c8c5ca07e1ada028
hadi14250 <hadihk2k@gmail.com>
2024-02-01 17:38:41 +0400

    done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-
+class CopyToClipboard {
+	constructor() {
+	  this.textToCopy = document.getElementById("textToCopy");
+	  this.copyButton = document.getElementById("copyButton");
+	  this.initialize();
+	}
+  
+	initialize() {
+	  this.copyButton.addEventListener("click", () => this.copyText());
+	}
+  
+	async copyText() {
+	  try {
+		await navigator.clipboard.writeText(this.textToCopy.textContent);
+		this.updateButtonText("COPIED!");
+	  } catch (err) {
+		console.error("Unable to copy text: ", err);
+	  }
+	}
+  
+	updateButtonText(text) {
+	  this.copyButton.textContent = text;
+  
+	  setTimeout(() => {
+		this.copyButton.textContent = "Copy Ca";
+	  }, 1500);
+	}
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+	new CopyToClipboard();
+  });
